52c0333863efce0945a398630dc8a196f21c194a
Sunny <thstkd92@gmail.com>
2022-03-02 15:06:01 +0900

    [Java] 2-8. 등수구하기

---
+++ algorithms/javaalgo/src/main/java/inflearn/chapter2/Solution8.java
@@ -0,0 +1,43 @@
+package inflearn.chapter2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 8. 등수구하기
+public class Solution8 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    String str = br.readLine();
+
+    int[] answers = solution(str);
+    for (int answer : answers) {
+      System.out.print(answer + " ");
+    }
+  }
+
+  private static int[] solution(String str) {
+    String[] splits = str.split(" ");
+    int[] scores = new int[splits.length];
+    for (int i = 0; i < splits.length; i++) {
+      scores[i] = Integer.parseInt(splits[i]);
+    }
+
+    int[] answers = new int[scores.length];
+
+    for (int i = 0; i < answers.length; i++) {
+      int score = scores[i];
+      int count = 0;
+      for (int j = 0; j < scores.length; j++) {
+        if (score < scores[j]) {
+          count++;
+        }
+      }
+      answers[i] = count + 1;
+    }
+
+    return answers;
+  }
+}
