163c7c4ab63966727161e667de9fe12e6968c0b5
Sunny <thstkd92@gmail.com>
2022-04-07 21:40:04 +0900

    [Java] 5-5. 쇠막대기

---
+++ algorithms/javaalgo/src/main/java/inflearn/chapter5/Solution5.java
@@ -0,0 +1,38 @@
+package inflearn.chapter5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+// 5. 쇠막대기
+// ()(((()())(())()))(())
+public class Solution5 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+
+    System.out.println(solution(str));
+  }
+
+  private static int solution(String str) {
+    int answer = 0;
+    Stack<Character> stack = new Stack<>();
+    char[] chars = str.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      if (chars[i] == '(') {
+        stack.push(chars[i]);
+      } else {
+        stack.pop();
+        if (chars[i - 1] == '(') {
+          answer += stack.size();
+        } else {
+          answer++;
+        }
+      }
+    }
+
+    return answer;
+  }
+}
