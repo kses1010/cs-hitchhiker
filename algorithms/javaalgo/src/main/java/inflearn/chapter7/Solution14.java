f10c5d76fc0fa3495fa0f2a78be8b51351351538
Sunny <thstkd92@gmail.com>
2022-04-13 17:02:30 +0900

    [Java] 7-14. 그래프 최단거리(BFS)

--- algorithms/javaalgo/src/main/java/inflearn/chapter7/Solution14.java
+++ algorithms/javaalgo/src/main/java/inflearn/chapter7/Solution14.java
@@ -1,41 +1,42 @@
-package inflearn.chapter7.solution11;
+package inflearn.chapter7;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
-// 13. 경로탐색(인접리스트, ArrayList) - 정점갯수가 많을 경우에 사용
-//5 9
-//1 2
+// 14. 그래프 최단거리(BFS)
+//6 9
 //1 3
 //1 4
 //2 1
-//2 3
 //2 5
 //3 4
-//4 2
 //4 5
-public class Solution13 {
+//4 6
+//6 2
+//6 5
+public class Solution14 {
   static int n = 0;
   static int m = 0;
-  static int answer = 0;
   static List<List<Integer>> graph;
   static int[] checks;
+  static int[] dist;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String[] splits = br.readLine().split(" ");
     n = Integer.parseInt(splits[0]);
     m = Integer.parseInt(splits[1]);
-
-    // 인접리스트
     graph = new ArrayList<>();
     for (int i = 0; i <= n; i++) {
       graph.add(new ArrayList<>());
     }
     checks = new int[n + 1];
+    dist = new int[n + 1];
     for (int i = 0; i < m; i++) {
       String[] coordinates = br.readLine().split(" ");
       int a = Integer.parseInt(coordinates[0]);
@@ -43,20 +44,25 @@ public class Solution13 {
       graph.get(a).add(b);
     }
 
-    checks[1] = 1;
-    dfs(1);
-    System.out.println(answer);
+    bfs(1);
+    for (int i = 2; i <= n; i++) {
+      System.out.println(i + " : " + dist[i]);
+    }
   }
 
-  private static void dfs(int v) {
-    if (v == n) {
-      answer++;
-    } else {
-      for (int nv : graph.get(v)) {
+  private static void bfs(int v) {
+    Queue<Integer> q = new LinkedList<>();
+    checks[v] = 1;
+    dist[v] = 1;
+    q.offer(v);
+
+    while (!q.isEmpty()) {
+      int cv = q.poll();
+      for (int nv : graph.get(cv)) {
         if (checks[nv] == 0) {
           checks[nv] = 1;
-          dfs(nv);
-          checks[nv] = 0;
+          q.offer(nv);
+          dist[nv] = dist[cv] + 1;
         }
       }
     }
