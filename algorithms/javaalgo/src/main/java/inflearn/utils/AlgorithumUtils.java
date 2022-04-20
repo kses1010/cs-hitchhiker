96a836df34fe1015aa53f274c8bbd92aa7c6e772
Sunny <thstkd92@gmail.com>
2022-04-20 16:08:03 +0900

    [Java] 조합 메모이제이션 Utils 추가

--- algorithms/javaalgo/src/main/java/inflearn/utils/AlgorithumUtils.java
+++ algorithms/javaalgo/src/main/java/inflearn/utils/AlgorithumUtils.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 
 public class AlgorithumUtils {
 
+  // 2차원 배열
   private static void twoDArr() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
@@ -16,6 +17,29 @@ public class AlgorithumUtils {
       for (int j = 0; j < n; j++) {
         arr[i][j] = Integer.parseInt(str[j]);
       }
+    }
+  }
+
+  // 조합(메모이제이션)
+  static int[][] dy = new int[35][35];
+
+  public static void combi() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] splits = br.readLine().split(" ");
+    int n = Integer.parseInt(splits[0]);
+    int r = Integer.parseInt(splits[1]);
+
+    System.out.println(dfs(n, r));
+  }
+
+  private static int dfs(int n, int r) {
+    if (dy[n][r] > 0) {
+      return dy[n][r];
+    }
+    if (n == r || r == 0) {
+      return 1;
+    } else {
+      return dy[n][r] = dfs(n - 1, r - 1) + dfs(n - 1, r);
     }
   }
 }
