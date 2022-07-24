49bc37a3aae7316ec9670c50ad4a6137d6a1130f
Sunny <thstkd92@gmail.com>
2022-07-24 23:19:04 +0900

    [Java] 1. Two Sum

---
+++ algorithms/javaalgo/src/test/java/leetcode/number1/TwoSumTest.java
@@ -0,0 +1,31 @@
+package leetcode.number1;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+// 1.Two Sum
+public class TwoSumTest {
+
+  private static int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int diff = target - nums[i];
+      if (map.containsKey(diff)) {
+        return new int[]{map.get(diff), i};
+      }
+      map.put(nums[i], i);
+    }
+    return null;
+  }
+
+  @Test
+  void test() {
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+
+    assertThat(twoSum(nums, target)).isEqualTo(new int[]{0, 1});
+  }
+}
