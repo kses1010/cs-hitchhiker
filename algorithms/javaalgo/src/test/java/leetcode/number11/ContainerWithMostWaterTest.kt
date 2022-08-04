14063a53eb19c4c5c2c184338204e33538ac0876
Sunny <thstkd92@gmail.com>
2022-08-04 22:50:40 +0900

    [kotlin] 11. Container With Most Water

---
+++ algorithms/javaalgo/src/test/java/leetcode/number11/ContainerWithMostWaterTest.kt
@@ -0,0 +1,42 @@
+package leetcode.number11
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import kotlin.math.max
+import kotlin.math.min
+
+// 11. Container With Most Water
+class ContainerWithMostWaterTest {
+
+    @Test
+    fun test() {
+        val height1: IntArray = intArrayOf(1, 8, 6, 2, 5, 4, 8, 3, 7)
+        val height2 = intArrayOf(1, 1)
+
+        assertAll(
+            { assertThat(maxArea(height1)).isEqualTo(49) },
+            { assertThat(maxArea(height2)).isEqualTo(1) },
+        )
+    }
+
+    private fun maxArea(height: IntArray): Int {
+        var left = 0
+        var right = height.size - 1
+        var max = 0
+        while (left < right) {
+            val width = right - left
+            val h = min(height[left], height[right])
+            val area = width * h
+            max = max(max, area)
+
+            if (height[left] < height[right]) left++
+            else if (height[left] > height[right]) right--
+            else {
+                left++
+                right--
+            }
+        }
+        return max
+    }
+}
