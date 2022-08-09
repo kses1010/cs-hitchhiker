eedefef5d8954a03cb4574c5cd659d0b23250064
Sunny <thstkd92@gmail.com>
2022-08-09 23:53:24 +0900

    [kotlin] 15. 3Sum - (temp)

---
+++ algorithms/javaalgo/src/test/java/leetcode/number15/ThreeSumTest.kt
@@ -0,0 +1,18 @@
+package leetcode.number15
+
+import org.junit.jupiter.api.Test
+
+// 15. 3Sum
+class ThreeSumTest {
+
+    @Test
+    fun test() {
+
+    }
+
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        val answerList: MutableList<List<Int>> = mutableListOf()
+        nums.sort()
+
+    }
+}
