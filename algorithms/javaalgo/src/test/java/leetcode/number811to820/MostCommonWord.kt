35e401b1ba14150fd57bdf79e48a913bdf140edd
Sunny <thstkd92@gmail.com>
2022-08-18 22:55:48 +0900

    [kotlin] 819. Most Common Word

---
+++ algorithms/javaalgo/src/test/java/leetcode/number811to820/MostCommonWord.kt
@@ -0,0 +1,31 @@
+package leetcode.number811to820
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class MostCommonWord {
+
+    @Test
+    fun test() {
+        val paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+        val banned = arrayOf("hit")
+
+        assertThat(mostCommonWord(paragraph, banned)).isEqualTo("ball")
+    }
+
+    fun mostCommonWord(paragraph: String, banned: Array<String>): String {
+        val wordMap: MutableMap<String, Int> = mutableMapOf<String, Int>().withDefault { 0 }
+        val regex = Regex("[^a-zA-Z]")
+        val str = paragraph.replace(regex, " ").lowercase()
+        val split: List<String> = str.split(" ")
+        val bannedSet = banned.toSet()
+
+        for (word in split) {
+            if (!bannedSet.contains(word) && word.isNotBlank()) {
+                wordMap[word] = wordMap.getValue(word) + 1
+            }
+        }
+        val max: Map.Entry<String, Int> = wordMap.maxBy { it.value }
+        return max.key
+    }
+}
