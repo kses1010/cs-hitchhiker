73d5ff0f7f2b15df4b671e5ebd552583256e2d03
Sunny <thstkd92@gmail.com>
2022-08-27 21:44:11 +0900

    [Java] 561. ArrayPartition

---
+++ algorithms/javaalgo/src/test/java/leetcode/number561to570/number561/ArrayPartitionTest.java
@@ -0,0 +1,30 @@
+package leetcode.number561to570.number561;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+public class ArrayPartitionTest {
+
+    @Test
+    void test() {
+        int[] nums1 = {1, 4, 3, 2};
+        int[] nums2 = {6, 2, 6, 5, 1, 2};
+
+        assertThat(arrayPairSum(nums1)).isEqualTo(4);
+        assertThat(arrayPairSum(nums2)).isEqualTo(9);
+    }
+
+    public int arrayPairSum(int[] nums) {
+        int answer = 0;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                answer += nums[i];
+            }
+        }
+        return answer;
+    }
+}
