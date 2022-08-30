15251090b265e553b279d47d9f33ce773b3e2684
Sunny <thstkd92@gmail.com>
2022-08-30 23:08:59 +0900

    [Java] 238. Product of Array Except Self

---
+++ algorithms/javaalgo/src/test/java/leetcode/number231to240/ProductOfArrayExceptSelfTest.java
@@ -0,0 +1,37 @@
+package leetcode.number231to240;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductOfArrayExceptSelfTest {
+
+    @Test
+    void test() {
+        int[] nums1 = {1, 2, 3, 4};
+        int[] nums2 = {-1, 1, 0, -3, 3};
+
+        assertAll(
+            () -> assertThat(productExceptSelf(nums1)).isEqualTo(new int[]{24, 12, 8, 6}),
+            () -> assertThat(productExceptSelf(nums2)).isEqualTo(new int[]{0, 0, 9, 0, 0})
+        );
+    }
+
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] answer = new int[length];
+
+        int p = 1;
+        for (int i = 0; i < length; i++) {
+            answer[i] = p;
+            p *= nums[i];
+        }
+        p = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            answer[i] *= p;
+            p *= nums[i];
+        }
+        return answer;
+    }
+}
