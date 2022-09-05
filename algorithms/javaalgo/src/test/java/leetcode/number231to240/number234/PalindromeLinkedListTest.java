a818c9e0172bbb94a307f9b059d80ccbb7aa3f10
Sunny <thstkd92@gmail.com>
2022-09-05 19:01:49 +0900

    [Java] 234. Palindrome Linked List

--- algorithms/javaalgo/src/test/java/leetcode/number231to240/number234/PalindromeLinkedListTest.java
+++ algorithms/javaalgo/src/test/java/leetcode/number231to240/number234/PalindromeLinkedListTest.java
@@ -1,5 +1,8 @@
 package leetcode.number231to240.number234;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import leetcode.util.ListNode;
 import org.junit.jupiter.api.Test;
 
 public class PalindromeLinkedListTest {
@@ -9,5 +12,25 @@ public class PalindromeLinkedListTest {
 
     }
 
+    public boolean isPalindrome(ListNode head) {
+        Deque<Integer> q = new LinkedList<>();
+        if (head == null) {
+            return true;
+        }
 
+        ListNode node = head;
+        while (node != null) {
+            q.add(node.val);
+            node = node.next;
+        }
+
+        while (q.size() > 1) {
+            int left = q.pollFirst();
+            int right = q.pollLast();
+            if (left != right) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- algorithms/javaalgo/src/test/java/leetcode/util/ListNode.java
+++ algorithms/javaalgo/src/test/java/leetcode/util/ListNode.java
@@ -2,8 +2,8 @@ package leetcode.util;
 
 public class ListNode {
 
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     public ListNode() {
     }
