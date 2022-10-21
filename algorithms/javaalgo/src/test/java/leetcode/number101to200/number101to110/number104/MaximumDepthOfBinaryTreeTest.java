48ddb8d75135d10c8630c32ffb648e689abc4c7b
Sunny <thstkd92@gmail.com>
2022-10-21 14:32:06 +0900

    [Java] 104. Maximum Depth Of Binary Tree

---
+++ algorithms/javaalgo/src/test/java/leetcode/number101to200/number101to110/number104/MaximumDepthOfBinaryTreeTest.java
@@ -0,0 +1,34 @@
+package leetcode.number101to200.number101to110.number104;
+
+public class MaximumDepthOfBinaryTreeTest {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
