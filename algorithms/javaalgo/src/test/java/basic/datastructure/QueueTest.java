7ee5b88f2a425d5e8a0a252f82a63435f32d6c86
Sunny <thstkd92@gmail.com>
2022-10-31 19:38:51 +0900

    refactor : 테스트 이름 변경 - 큐 테스트는 전부 QueueTest 클래스에서 하도록 변경

--- algorithms/javaalgo/src/test/java/basic/datastructure/QueueTest.java
+++ algorithms/javaalgo/src/test/java/basic/datastructure/QueueTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.Test;
 
-public class StackQueueTest {
+public class QueueTest {
 
     @Test
     void queueTest() {
