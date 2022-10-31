de717041649507ae968d0a2ee83e4c0f80d6fbe3
Sunny <thstkd92@gmail.com>
2022-10-31 19:47:29 +0900

    [Java] Live 코딩테스트 대비 - 순환 큐 구현

---
+++ algorithms/javaalgo/src/test/java/basic/datastructure/CircularQueue.java
@@ -0,0 +1,63 @@
+package basic.datastructure;
+
+public class CircularQueue {
+
+    private int head;
+    private int tail;
+    private int size;
+    private int[] arr;
+
+    public CircularQueue(int k) {
+        this.head = -1;
+        this.tail = -1;
+        this.size = k;
+        this.arr = new int[k];
+    }
+
+    public boolean isEmpty() {
+        return tail == -1;
+    }
+
+    public boolean isFull() {
+        return ((head + 1) % size) == tail;
+    }
+
+    public boolean enQueue(int element) {
+        if (isFull()) {
+            return false;
+        }
+        if (isEmpty()) {
+            tail = 0;
+        }
+        head = (head + 1) % size;
+        arr[head] = element;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        if (head == tail) {
+            head = -1;
+            tail = -1;
+            return true;
+        }
+        tail = (tail + 1) % size;
+        return true;
+    }
+
+    public int front() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return arr[head];
+    }
+
+    public int rear() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return arr[tail];
+    }
+}
--- algorithms/javaalgo/src/test/java/basic/datastructure/QueueTest.java
+++ algorithms/javaalgo/src/test/java/basic/datastructure/QueueTest.java
@@ -3,11 +3,13 @@ package basic.datastructure;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class QueueTest {
 
     @Test
+    @DisplayName("스택으로 큐 구현")
     void queueTest() {
         StackQueue<Integer> queue = new StackQueue<>();
         queue.add(10);
@@ -24,5 +26,46 @@ public class QueueTest {
         queue.add(80);
 
         assertThat(queue.poll()).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("순환 큐 구현")
+    void circularQueueTest() {
+        CircularQueue queue = new CircularQueue(5);
+        assertAll(
+            () -> assertThat(queue.isEmpty()).isTrue(),
+            () -> assertThat(queue.isFull()).isFalse()
+        );
+
+        queue.enQueue(1);
+        queue.enQueue(10);
+        queue.enQueue(20);
+        queue.enQueue(30);
+        queue.enQueue(40);
+        assertAll(
+            () -> assertThat(queue.isFull()).isTrue(),
+            () -> assertThat(queue.enQueue(100)).isFalse(),
+            () -> assertThat(queue.front()).isEqualTo(40),
+            () -> assertThat(queue.rear()).isEqualTo(1)
+        );
+
+        queue.deQueue();
+        queue.deQueue();
+        assertThat(queue.rear()).isEqualTo(20);
+
+        queue.enQueue(50);
+        queue.enQueue(60);
+        assertThat(queue.front()).isEqualTo(60);
+
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+        assertAll(
+            () -> assertThat(queue.isEmpty()).isTrue(),
+            () -> assertThat(queue.front()).isEqualTo(-1),
+            () -> assertThat(queue.rear()).isEqualTo(-1)
+        );
     }
 }
