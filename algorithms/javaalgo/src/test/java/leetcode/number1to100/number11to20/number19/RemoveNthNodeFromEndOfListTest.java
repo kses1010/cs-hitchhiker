121c37b85d11b48e608fc6e28baae0a7db90d16b
Sunny <thstkd92@gmail.com>
2022-12-12 19:16:35 +0900

    [Java] 19. Remove Nth Node From End Of List

---
+++ algorithms/javaalgo/src/test/java/leetcode/number1to100/number11to20/number19/RemoveNthNodeFromEndOfListTest.java
@@ -0,0 +1,25 @@
+package leetcode.number1to100.number11to20.number19;
+
+import leetcode.util.ListNode;
+
+public class RemoveNthNodeFromEndOfListTest {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode(0);
+        ListNode slow = start;
+        ListNode fast = start;
+        slow.next = head;
+
+        for (int i = 1; i <= n + 1; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return start.next;
+    }
+}
