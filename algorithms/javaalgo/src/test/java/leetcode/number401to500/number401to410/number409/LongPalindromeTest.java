1251bdc17f9d65c3da9d2750df87f62e51ae6fe3
Sunny <thstkd92@gmail.com>
2023-01-23 14:25:15 +0900

    [Java] 409. Long Palindrome - 새로운 풀이 추가

--- algorithms/javaalgo/src/test/java/leetcode/number401to500/number401to410/number409/LongPalindromeTest.java
+++ algorithms/javaalgo/src/test/java/leetcode/number401to500/number401to410/number409/LongPalindromeTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 public class LongPalindromeTest {
 
     @Test
-    void test() {
+    void test1() {
         String s1 = "abccccdd";
         String s2 = "a";
         String s3 = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
@@ -39,6 +39,40 @@ public class LongPalindromeTest {
             }
         }
 
+        return answer;
+    }
+
+    @Test
+    void test2() {
+        String s1 = "abccccdd";
+        String s2 = "a";
+        String s3 = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+
+        assertAll(
+            () -> assertThat(longPalindrome2(s1)).isEqualTo(7),
+            () -> assertThat(longPalindrome2(s2)).isEqualTo(1),
+            () -> assertThat(longPalindrome2(s3)).isEqualTo(983)
+        );
+    }
+
+    public int longPalindrome2(String s) {
+        int answer = 0;
+        Map<Character, Integer> maps = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            maps.put(c, maps.getOrDefault(c, 0) + 1);
+        }
+        boolean isOdd = false;
+        for (int count : maps.values()) {
+            if (count % 2 == 0) {
+                answer += count;
+            } else {
+                answer += count - 1;
+                isOdd = true;
+            }
+        }
+        if (isOdd) {
+            return answer + 1;
+        }
         return answer;
     }
 }
