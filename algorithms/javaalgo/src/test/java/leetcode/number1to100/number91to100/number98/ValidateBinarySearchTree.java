84ba39c2057f1cf2fe429a513243bdc21e0edfc7
Sunny <thstkd92@gmail.com>
2023-01-30 12:54:31 +0900

    [Java] 98. Validate Binary Search Tree

---
+++ algorithms/javaalgo/src/test/java/leetcode/number1to100/number91to100/number98/ValidateBinarySearchTree.java
@@ -0,0 +1,20 @@
+package leetcode.number1to100.number91to100.number98;
+
+import leetcode.util.TreeNode;
+
+public class ValidateBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val >= maxVal || root.val <= minVal) {
+            return false;
+        }
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+}
