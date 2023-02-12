c533e89fe82f98a5529303723db29f84eb37de40
Sunny <thstkd92@gmail.com>
2023-02-12 13:36:09 +0900

    [Java] 160. Intersection Of Two Linked Lists

---
+++ algorithms/javaalgo/src/test/java/leetcode/number101to200/number151to160/number160/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,28 @@
+package leetcode.number101to200.number151to160.number160;
+
+import leetcode.util.ListNode;
+
+public class IntersectionOfTwoLinkedLists {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            if (a == null) {
+                a = headB;
+            } else {
+                a = a.next;
+            }
+            if (b == null) {
+                b = headA;
+            } else {
+                b = b.next;
+            }
+        }
+        return a;
+    }
+}
