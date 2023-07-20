7b77f73d635cc13c0f513426ffe9fd9e542711d8
Sunny <thstkd92@gmail.com>
2023-07-20 16:24:35 +0900

    refactor : 패키지 이동

--- algorithms/javaalgo/src/test/java/programmers/level1/failurerate/FailureRateTest.java
+++ algorithms/javaalgo/src/test/java/programmers/level1/failurerate/FailureRateTest.java
@@ -1,4 +1,4 @@
-package programmers.level3.failurerate;
+package programmers.level1.failurerate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
