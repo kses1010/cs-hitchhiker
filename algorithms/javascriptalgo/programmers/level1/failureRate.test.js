f2ff30dbf66c2f60098cddab8f0abb58157ca461
Sunny <thstkd92@gmail.com>
2023-08-02 00:14:48 +0900

    [Javascript] Programmers Level 1. 실패율

---
+++ algorithms/javascriptalgo/programmers/level1/failureRate.test.js
@@ -0,0 +1,21 @@
+test("test", () => {
+    var N1 = 5;
+    var stages1 = [2, 1, 2, 6, 2, 4, 3, 3];
+
+    var N2 = 4;
+    var stages2 = [4, 4, 4, 4, 4];
+
+    expect(solution(N1, stages1)).toEqual([3, 4, 2, 1, 5]);
+    expect(solution(N2, stages2)).toEqual([4, 1, 2, 3]);
+});
+
+function solution(N, stages) {
+    let result = [];
+    for (let i = 1; i <= N; i++) {
+        let arrival = stages.filter((x) => x >= i).length; // 해당 스테이지 통과 및 멈춘 수
+        let cur = stages.filter((x) => x === i).length; // 해당 스테이지에 머문 수
+        result.push([i, cur / arrival]);
+    }
+    result.sort((a, b) => b[1] - a[1]);
+    return result.map((x) => x[0]);
+}
