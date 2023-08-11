e10297f9354b8a064d7dda4e32473773d22972f1
Sunny <thstkd92@gmail.com>
2023-08-11 17:25:58 +0900

    [Javascript] Programmers Level 3. 정수 삼각형 - 함수형으로 처리하기

--- algorithms/javascriptalgo/programmers/level3/intergerTriangle.test.js
+++ algorithms/javascriptalgo/programmers/level3/intergerTriangle.test.js
@@ -24,3 +24,11 @@ function solution(triangle) {
 
     return answer;
 }
+
+function solution2(triangle) {
+    return Math.max(...triangle.reduce((cost, line) => {
+        return line.map((v, index) => {
+            return v + Math.max((index < cost.length ? cost[index] : 0), (index > 0 ? cost[index - 1] : 0));
+        });
+    }, []));
+}
