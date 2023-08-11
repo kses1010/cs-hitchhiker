11666b1257248a55f04a8669f482d0b4adeb1ae5
Sunny <thstkd92@gmail.com>
2023-08-11 19:27:05 +0900

    [Javascript] Programmers Level 3. 네트워크

---
+++ algorithms/javascriptalgo/programmers/level3/network.test.js
@@ -0,0 +1,30 @@
+test("test", () => {
+    const computers1 = [[1, 1, 0], [1, 1, 0], [0, 0, 1]];
+    const computers2 = [[1, 1, 0], [1, 1, 1], [0, 1, 1]];
+
+    expect(solution(3, computers1)).toBe(2);
+    expect(solution(3, computers2)).toBe(1);
+})
+
+function solution(n, computers) {
+    let answer = 0;
+    const visited = new Array(computers.length).fill(false);
+
+    for (let i = 0; i < computers.length; i++) {
+        if (!visited[i]) {
+            answer++;
+            dfs(i, computers);
+        }
+    }
+
+    function dfs(start, computers) {
+        visited[start] = true;
+        for (let i = 0; i < computers.length; i++) {
+            if (!visited[i] && computers[start][i] === 1) {
+                dfs(i, computers);
+            }
+        }
+    }
+
+    return answer;
+}
