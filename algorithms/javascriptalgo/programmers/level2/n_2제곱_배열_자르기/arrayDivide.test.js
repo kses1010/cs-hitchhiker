9472e1175f9ea8d24c35ca8081a2be56256838de
sunny-son <thstkd92@gmail.com>
2024-11-21 09:53:25 +0900

    [Javascript] programmers - level2. n^2 배열 자르기

--- algorithms/javascriptalgo/programmers/level2/n_2제곱_배열_자르기/arrayDivide.test.js
+++ algorithms/javascriptalgo/programmers/level2/n_2제곱_배열_자르기/arrayDivide.test.js
@@ -13,7 +13,7 @@ test("test", () => {
 
 function solution(n, left, right) {
     let answer = [];
-    for (let i = left; i < right + 1; i++) {
+    for (let i = left; i <= right; i++) {
         let row = Math.floor(i / n);
         let col = i % n;
         answer.push(Math.max(row, col) + 1);
