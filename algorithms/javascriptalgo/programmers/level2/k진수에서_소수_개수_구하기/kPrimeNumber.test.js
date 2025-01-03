c65c5c246559f8ad7d179f855729c752e4938507
sunny-son <thstkd92@gmail.com>
2025-01-03 19:31:48 +0900

    [Javascript] programmers - level2. k진수에서 소수 개수 구하기

---
+++ algorithms/javascriptalgo/programmers/level2/k진수에서_소수_개수_구하기/kPrimeNumber.test.js
@@ -0,0 +1,36 @@
+test("test", () => {
+  let n1 = 437674;
+  let k1 = 3;
+
+  let n2 = 110011;
+  let k2 = 10;
+
+  expect(solution(n1, k1)).toBe(3);
+  expect(solution(n2, k2)).toBe(2);
+});
+
+function solution(n, k) {
+  let answer = 0;
+
+  function isPrime(num) {
+    if (!num || num === 1) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  let kNum = n.toString(k);
+  let splits = kNum.split("0");
+
+  for (const num of splits) {
+    if (isPrime(Number(num))) {
+      answer++;
+    }
+  }
+  return answer;
+}
